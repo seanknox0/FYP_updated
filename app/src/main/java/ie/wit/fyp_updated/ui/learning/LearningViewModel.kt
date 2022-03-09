1ea19fa1dac516297114b0ca2ec831a910896ac2
seanknox0 <seanknox0@gmail.com>
2022-03-09 14:05:16 +0000

    Menu functioning and navigating to fragments and activitys accordingly. UI problems fixed. Personal section is WIP.

--- app/src/main/java/ie/wit/fyp_updated/ui/learning/LearningViewModel.kt
+++ app/src/main/java/ie/wit/fyp_updated/ui/learning/LearningViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class LearningViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is Learning Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/ie/wit/fyp_updated/ui/personal/PersonalActivity.kt
+++ app/src/main/java/ie/wit/fyp_updated/ui/personal/PersonalActivity.kt
@@ -2,11 +2,28 @@ package ie.wit.fyp_updated.ui.personal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.navigation.ui.AppBarConfiguration
 import ie.wit.fyp_updated.R
+import ie.wit.fyp_updated.databinding.ActivityAccountBinding
+import ie.wit.fyp_updated.databinding.ActivityMainBinding
+import ie.wit.fyp_updated.databinding.ActivityPersonalBinding
 
 class PersonalActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityPersonalBinding
+
+    //ActionBar
+    private lateinit var actionBar: ActionBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_personal)
+        binding = ActivityPersonalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Configure ActionBar
+        actionBar = supportActionBar!!
+        actionBar.title = "Personal"
     }
 }
